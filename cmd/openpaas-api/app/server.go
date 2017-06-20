02b01cadc057ca3ef8367e7224e05ea51ed1f54a
xinzhiyun <xinzhiyun@xinzhiyundeMacBook-Pro.local>
2017-06-20 19:23:05 +0800

    init openpaas

--- cmd/openpaas-api/app/server.go
+++ cmd/openpaas-api/app/server.go
@@ -6,8 +6,11 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	v1 "k8s.io/client-go/pkg/api/v1"
+	v1beta1 "k8s.io/client-go/pkg/apis/apps/v1beta1"
+	v1beta11 "k8s.io/client-go/pkg/apis/extensions/v1beta1"
 	"net/http"
 	"fmt"
+	//"k8s.io/kubernetes/pkg/kubectl"
 )
 
 type KubetnetesCLient struct {
@@ -131,13 +134,14 @@ func (kc *KubetnetesCLient)CreateRC() {
 	fmt.Println(rc.Namespace)
 	return
 }
+
 func (kc *KubetnetesCLient)CreateService() {
-	serport:=v1.ServicePort{
+	serport := v1.ServicePort{
 		Name:"mysqlport",
 		Protocol:v1.ProtocolTCP,
 		Port:7878,
 	}
-	service,err:=kc.KubeCtl.CoreV1().Services("qinzhao").Create(&v1.Service{
+	service, err := kc.KubeCtl.CoreV1().Services("qinzhao").Create(&v1.Service{
 		TypeMeta:metav1.TypeMeta{
 			Kind:"Service",
 			APIVersion:"v1",
@@ -153,8 +157,8 @@ func (kc *KubetnetesCLient)CreateService() {
 			Ports:[]v1.ServicePort{serport},
 		},
 	})
-	if err!=nil{
-		glog.Errorf("%#v\n",err)
+	if err != nil {
+		glog.Errorf("%#v\n", err)
 		return
 	}
 	glog.Info(service)
@@ -162,14 +166,291 @@ func (kc *KubetnetesCLient)CreateService() {
 
 }
 
+func (kc *KubetnetesCLient)CreateDeployment() {
+
+	Container := v1.Container{
+		Name:"redis",
+		Image:"redis:latest",
+	}
+
+	dep := &v1beta1.Deployment{
+
+		TypeMeta:metav1.TypeMeta{
+			Kind:"Deployment",
+			APIVersion:"v1beta1",
+		},
+		ObjectMeta:metav1.ObjectMeta{
+			Name:"qinzhao",
+			Namespace:"qinzhao",
+		},
+		Spec:v1beta1.DeploymentSpec{
+			Replicas:Int32Toint32Point(1),
+			Selector:&metav1.LabelSelector{
+				MatchLabels:map[string]string{
+					"qinzhao":"qinzhao",
+				},
+			},
+			Template:v1.PodTemplateSpec{
+				ObjectMeta:metav1.ObjectMeta{
+					Name:"qinzhao",
+					Namespace:"qinzhao",
+					Labels:map[string]string{
+						"qinzhao":"qinzhao",
+					},
+				},
+				Spec:v1.PodSpec{
+					Containers:[]v1.Container{Container},
+				},
+			},
+
+		},
+	}
+
+	deployment1, err := kc.KubeCtl.AppsV1beta1Client.Deployments("qinzhao").Create(dep)
+	if err != nil {
+		glog.Errorf("create deployment failed%#v\n", err)
+		return
+	}
+
+	fmt.Println(deployment1)
+	return
+}
+
+func (kc *KubetnetesCLient)CreateDaemonSets() {
+	Container := v1.Container{
+		Name:"redis",
+		Image:"redis:latest",
+		ImagePullPolicy:v1.PullAlways,
+	}
+	daemonSets := &v1beta11.DaemonSet{
+		TypeMeta:metav1.TypeMeta{
+			Kind:"DaemonSet",
+			APIVersion:"v1beta1",
+		},
+		ObjectMeta:metav1.ObjectMeta{
+			Name:"qinzhao",
+			Namespace:"qinzhao",
+			Labels:map[string]string{
+				"qinzhao":"qinzhao",
+			},
+		},
+		Spec:v1beta11.DaemonSetSpec{
+			Selector:&metav1.LabelSelector{
+				MatchLabels:map[string]string{"qinzhao":"qinzhao"},
+			},
+			Template:v1.PodTemplateSpec{
+				ObjectMeta:metav1.ObjectMeta{
+					Namespace:"qinzhao",
+					Name:"qinzhao",
+					Labels:map[string]string{
+						"qinzhao":"qinzhao",
+					},
+				},
+				Spec:v1.PodSpec{
+					Containers:[]v1.Container{Container},
+				},
+			},
+		},
+
+	}
+
+	daemonset, err := kc.KubeCtl.DaemonSets("qinzhao").Create(daemonSets)
+	if err != nil {
+		glog.Errorf("%#s\n", err)
+		return
+	}
+	fmt.Println(daemonset)
+	return
+
+}
+
+func (kc *KubetnetesCLient) GetDaemonSets() {
+	//opention:=v1.
+	daemonset, err := kc.KubeCtl.DaemonSets("qinzhao").Get("qinzhao", metav1.GetOptions{
+		TypeMeta:metav1.TypeMeta{
+			Kind:"DaemonSet",
+			APIVersion:"v1beta1",
+		},
+	})
+	if err != nil {
+		glog.Errorf("get daemonSets failed", err)
+		return
+	}
+	fmt.Println(daemonset.Name)
+	return
+
+}
+
+func (kc *KubetnetesCLient)GetService() {
+	service, err := kc.KubeCtl.CoreV1().Services("qinzhao").Get("qinzhao", metav1.GetOptions{
+		TypeMeta:metav1.TypeMeta{
+			Kind:"Service",
+			APIVersion:"v1",
+		},
+	})
+	if err != nil {
+		glog.Errorf("get service failed%#v\n", service)
+		return
+	}
+	fmt.Println(service.Spec.ClusterIP)
+}
+
+func (kc *KubetnetesCLient)GetDeployment() {
+	deployment, err := kc.KubeCtl.AppsV1beta1Client.Deployments("qinzhao").Get("qinzhao", metav1.GetOptions{
+		TypeMeta:metav1.TypeMeta{
+			Kind:"Deployment",
+			APIVersion:"v1beta1",
+		},
+	})
+
+	if err != nil {
+		glog.Errorf("get deploymnet failed%#v\n", err)
+
+		return
+	}
+
+	fmt.Println(deployment.Spec.Template)
+	return
+}
+
+func (kc *KubetnetesCLient)GetRC() {
+
+	rc, err := kc.KubeCtl.CoreV1().ReplicationControllers("qinzhao").Get("qinzhao",
+		metav1.GetOptions{
+			TypeMeta:metav1.TypeMeta{
+				Kind:"ReplicationController",
+				APIVersion:"v1",
+			},
+		})
+	if err != nil {
+		glog.Errorf("%#v\n", err)
+		return
+	}
+
+	fmt.Println(rc.ResourceVersion)
+
+}
+
+func (kc *KubetnetesCLient)UpdateNamespace() {
+	namespace := &v1.Namespace{
+		TypeMeta:metav1.TypeMeta{
+			Kind:"Namespace",
+			APIVersion:"v1",
+		},
+		ObjectMeta:metav1.ObjectMeta{
+			Name:"qinzhao",
+			Namespace:"qinzhao",
+			Annotations:map[string]string{
+				"qinzhao":"qinzhao",
+			},
+		},
+
+
+	}
+	name, err := kc.KubeCtl.CoreV1().Namespaces().Update(namespace)
+	if err != nil {
+		glog.Errorf("update namespace failed %#v\n", err)
+		return
+	}
+	fmt.Println(name.ResourceVersion)
+	return
+}
+
+func (kc *KubetnetesCLient)GetNameSpace() {
+
+	namespace, err := kc.KubeCtl.CoreV1().Namespaces().Get("qinzhao", metav1.GetOptions{
+		TypeMeta:metav1.TypeMeta{
+			Kind:"Namespace",
+			APIVersion:"v1",
+		},
+
+	})
+	if err != nil {
+		glog.Errorf("get namespace failed%#v\n", err)
+		return
+	}
+	fmt.Println(namespace.ResourceVersion)
+	return
+
+}
+
+func (kc *KubetnetesCLient)UpdateRC(){
+	Container := v1.Container{
+		Name:"redis",
+		Image:"redis:v1.2",
+		ImagePullPolicy:v1.PullAlways,
+	}
+	rc,err:=kc.KubeCtl.CoreV1().ReplicationControllers("qinzhao").Update(&v1.ReplicationController{
+		TypeMeta:metav1.TypeMeta{
+			Kind:"ReplicationController",
+			APIVersion:"v1",
+		},
+		ObjectMeta:metav1.ObjectMeta{
+			Namespace:"qinzhao",
+			Name:"qinzhao",
+			Annotations:map[string]string{
+			"qinzhao":"qinzhao",
+			},
+
+		},
+		Spec:v1.ReplicationControllerSpec{
+			Selector:map[string]string{
+				"qinzhao":"qinzhao",
+			},
+			Template:&v1.PodTemplateSpec{
+				ObjectMeta:metav1.ObjectMeta{
+					Name:"qinzhao",
+					Namespace:"qinzhao",
+					Labels:map[string]string{
+						"qinzhao":"qinzhao",
+					},
+				},
+				Spec:v1.PodSpec{
+					Containers:[]v1.Container{Container},
+				},
+			},
+		},
+	})
+
+	if err!=nil{
+		glog.Errorf("update ReplicationController failed %#v\n",err)
+		return
+	}
+	fmt.Println(rc.ResourceVersion)
+	return
+}
+
+//func (kc *KubetnetesCLient)UpdateDaemonSets(){
+//kc.KubeCtl
+//
+//}
+
+
+
 func Run() {
+
 	var kubecli KubetnetesCLient
+
 	kubecli.getKubeCli()
 	//kubecli.GetNodes()
-	kubecli.CreateNamespace()
+	//kubecli.CreateNamespace()
 	//kubecli.GetPods()
-	kubecli.CreateRC()
-	kubecli.CreateService()
+	//kubecli.CreateRC()
+	//kubecli.CreateDeployment()
+	//kubecli.CreateService()
+	//kubecli.CreateDaemonSets()
+
+	//kubecli.GetDaemonSets()
+
+	//kubecli.GetService()
+	//kubecli.GetDeployment()
+
+	//kubecli.GetRC()
+	//kubecli.GetNameSpace()
+	//kubecli.UpdateNamespace()
+	//kubecli.GetNameSpace()
+	kubecli.GetRC()
+	kubecli.UpdateRC()
 	http.ListenAndServe(fmt.Sprintf(":%d", 9090), nil)
 
 }
